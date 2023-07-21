f2d0399ce99d0aa3e739b0bae66f5bb6a22f4dd2
Fadyy22 <fadyalaa441@gmail.com>
2023-07-21 15:15:01 +0300

    enhancing bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -29,16 +29,21 @@ void swap(int *a, int *b)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	int flag;
 
 	for (i = 0; i < size; i++)
 	{
+		flag = 0;
 		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
+				flag = 1;
 			}
 		}
+		if (flag == 0)
+			break;
 	}
 }
